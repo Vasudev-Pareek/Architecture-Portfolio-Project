1d9410ed3978fec5322bfcd315314c009512cf51
Vasudev Pareek <vasudevpareek626@gmail.com>
2026-04-11 17:12:29 +0530

    Revert the cors headers

--- backend/server.js
+++ backend/server.js
@@ -9,22 +9,22 @@ const projectRoutes = require("./routes/projectRoutes");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// app.use(cors());
+app.use(cors());
 // const cors = require("cors");
 
-const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:5000",
-    "http://localhost:4000",
-    "https://architecture-portfolio-project.onrender.com"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-};
+// const corsOptions = {
+//   origin: [
+//     "http://localhost:5173",
+//     "http://localhost:5000",
+//     "http://localhost:4000",
+//     "https://architecture-portfolio-project.onrender.com"
+//   ],
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true
+// };
 
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // ✅ important
+// app.use(cors(corsOptions));
+// app.options("*", cors(corsOptions)); // ✅ important
 
 
 app.use(express.json());
